8f68d0d2bb7af2408350515abfcb54d986222c1c
DonKingFrog <65225182+DonKingFrog@users.noreply.github.com>
2023-09-14 22:16:42 +0200

    Update countdown.js

--- JavaScript/countdown.js
+++ JavaScript/countdown.js
@@ -1,4 +1,4 @@
-(function () {    
+(function () {
     const second = 1000,
             minute = second * 60,
             hour = minute * 60,
@@ -7,7 +7,7 @@
     const countDown = new Date("September 15, 2023 20:00:00").getTime(),
         x = setInterval(function() {    
 
-            const now = new Date("September 15, 2023 11:13:00").getTime(),
+            const now = new Date().getTime(),
                 distance = countDown - now;
 
             document.getElementById("days").innerText = Math.floor(distance / (day)),
