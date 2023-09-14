9d9e878ce83d7dd6b03892c6a3f83f5d91a2e4c8
DonKingFrog <65225182+DonKingFrog@users.noreply.github.com>
2023-09-14 22:02:15 +0200

    Update countdown.js

--- JavaScript/countdown.js
+++ JavaScript/countdown.js
@@ -4,7 +4,7 @@
             hour = minute * 60,
             day = hour * 24;
 
-    const countDown = new Date("09/15/2023").getTime(),
+    const countDown = new Date("09/15/2023").getTime(), countDown.hour += 20
         x = setInterval(function() {    
 
             const now = new Date().getTime(),
